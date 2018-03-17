049f7083edd6ed9dd6618975ef9046ea6b8056d2
or moyal <ormoyall@gmail.com>
2018-03-18 01:19:45 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,16 +5,6 @@ mongoose.Promise = global.Promise;
 var connectPath, options;
 //Check if we are on Heroku
 
-// mongoose.connect('mongodb://Admin:orAdmin123@ds115799.mlab.com:15799/mongodb-todo-app-api', {
-//     auth: {
-//         user: 'Admin',
-//         password: 'orAdmin123'
-//     }
-//   })
-//   .then(() => console.log('connection successful'))
-//   .catch((err) => console.error(err));
-
-
 if(process.env.PORT){
  connectPath = "mongodb://Admin:orAdmin123@ds115799.mlab.com:15799/mongodb-todo-app-api";
  options= {
@@ -28,11 +18,5 @@ if(process.env.PORT){
  options = {}
 }
 mongoose.connect(connectPath, options);
-
-console.log('process.env.MONGODB_URI ',process.env.MONGODB_URI);
-console.log('process.env.PORT ',process.env.PORT);
-
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAppDB');
 
 module.exports = {mongoose};
