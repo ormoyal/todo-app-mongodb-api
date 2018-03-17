3ee45f59a3ff0eeb8839465dea4880983f4bf17f
or moyal <ormoyall@gmail.com>
2018-03-18 01:05:53 +0200

    Setup app for herokuu

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,19 +4,30 @@ mongoose.Promise = global.Promise;
 
 var connectPath, options;
 //Check if we are on Heroku
-if(process.env.PORT){
- connectPath = "mongodb://<Admin>:<orAdmin123>@ds115799.mlab.com:15799/mongodb-todo-app-api";
- options= {
-     auth: {
-         user: 'Admin',
-         password: 'orAdmin123'
-     }
- }
-}else{
- connectPath = "mongodb://localhost:27017/TodoApp";
- options = {}
-}
-mongoose.connect(connectPath, options);
+
+mongoose.connect('mongodb://<Admin>:<orAdmin123>@ds115799.mlab.com:15799/mongodb-todo-app-api', {
+    auth: {
+        user: 'Admin',
+        password: 'orAdmin123'
+    }
+  })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+
+// if(process.env.PORT){
+//  connectPath = "mongodb://<Admin>:<orAdmin123>@ds115799.mlab.com:15799/mongodb-todo-app-api";
+//  options= {
+//      auth: {
+//          user: 'Admin',
+//          password: 'orAdmin123'
+//      }
+//  }
+// }else{
+//  connectPath = "mongodb://localhost:27017/TodoApp";
+//  options = {}
+// }
+// mongoose.connect(connectPath, options);
 
 // console.log('process.env ',process.env);
 
