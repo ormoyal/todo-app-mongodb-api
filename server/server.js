8a91a3b324a8ef2a5e122833a49e4e16bc60b892
or moyal <ormoyall@gmail.com>
2018-03-18 00:20:26 +0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const {Todo} = require('./models/Todo');
 
 var app = express();
 
-const port = process.env.port || 3000; 
+const port = process.env.PORT || 3000; 
 
 app.listen(port,() => {
     console.log(`started on port ${port} `);
